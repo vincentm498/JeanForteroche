bc8a0414145fa09363caf96503e2f76961d5eca3
Vincent Moreau <vincentm498@gmal.com>
2019-07-15 13:03:03 +0200

    modifs de css

--- view/include/head.php
+++ view/include/head.php
@@ -24,7 +24,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <link rel="stylesheet" href="assets/styles/css/header.css">
 <link rel="stylesheet" href="assets/styles/css/footer.css">
-<link rel="stylesheet" href="assets/styles/css/main.css">
+<link rel="stylesheet" href="assets/styles/css/content.css">
 <!-- <link rel="stylesheet" href="assets/styles/css/responsive.css"> -->
 <!-- <link rel="stylesheet" href="assets/styles/css/styles.css"> -->
 <link rel="stylesheet" href="assets/styles/css/style.css">
