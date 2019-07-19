34c05a62971b6721eec1a8460782f53374d18ead
Vincent Moreau <vincentm498@gmail.com>
2019-07-19 21:48:05 +0200

    Modifs

--- model/Connect_model.php
+++ model/Connect_model.php
@@ -5,8 +5,15 @@ namespace Blog\Model;
 class Connect_model
 {
     protected function dbConnect()
-        {
-            $db = new \PDO('mysql:host=localhost;dbname=jeanforteroche;charset=utf8', 'root', '');
-            return $db;
-        }
+    {
+        $db = new \PDO('mysql:host=localhost;dbname=jeanforteroche;charset=utf8', 'root', '');
+        return $db;
+    }
+}
+
+if (!$db) {
+    echo "Erreur : Impossible de se connecter à MySQL." . PHP_EOL;
+    echo "Errno de débogage : " . mysqli_connect_errno() . PHP_EOL;
+    echo "Erreur de débogage : " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
