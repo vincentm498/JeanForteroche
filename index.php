2a2b81f2211e0bd8559310651026e428585a8a99
Vincent Moreau <vincentm498@gmail.com>
2019-07-19 15:33:01 +0200

    modif de l'index

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 require('controller/frontend_controller.php');
 require('_functions/functions.php');
 
-if (isset($_GET['action']))
+$action = "";
+
+if (isset($_GET['action'])) {
     $action = $_GET['action'];
-else
-    $action = "Accueil";
+}
+
 
 switch ($action) {
 
