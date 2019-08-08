b54f324008d9a12c78735baad101693c05ee5cfd
Vincent Moreau <vincentm498@gmail.com>
2019-08-08 14:22:41 +0200

    Mise a jour

--- model/Articles.php
+++ model/Articles.php
@@ -1,34 +1,34 @@
 <?php
 
 
-// class Articles extends Connect
-// {
-//     //Renvoie la liste de tous les articles
-//     public function getAllArticles()
-//     {
-echo 'ok';
-//         $db = $this->dbConnect();
-//         $articles = $db->query('SELECT * FROM articles ORDER BY id ASC');
+class Articles
+{
+    //Renvoie la liste de tous les articles
+    public function getAllArticles()
+    {
+        echo 'ok';
+        // $db = $this->dbConnect();
+        // $articles = $db->query('SELECT * FROM articles ORDER BY id ASC');
 
-//         return $articles->fetchAll();
-//     }
+        // return $articles->fetchAll();
+    }
 
-//     // Renvoie la liste des deux derniers articles
-//     public function getLastArticles()
-//     {
-//         $db = $this->dbConnect();
-//         $lastArticles = $db->query('SELECT * FROM articles ORDER BY id DESC LIMIT 2');
+    // Renvoie la liste des deux derniers articles
+    public function getLastArticles()
+    {
+        $db = $this->dbConnect();
+        $lastArticles = $db->query('SELECT * FROM articles ORDER BY id DESC LIMIT 2');
 
-//         return $lastArticles->fetchAll();
-//     }
+        return $lastArticles->fetchAll();
+    }
 
-//     // Renvoie l'article
-//     public function getArticle($id)
-//     {
-//         $db = $this->dbConnect();
-//         $article = $db->prepare('SELECT * FROM articles WHERE id = ?');
-//         $article->execute(array($id));
+    // Renvoie l'article
+    public function getArticle($id)
+    {
+        $db = $this->dbConnect();
+        $article = $db->prepare('SELECT * FROM articles WHERE id = ?');
+        $article->execute(array($id));
 
-//         return $article->fetch();
-//     }
-// }
+        return $article->fetch();
+    }
+}
